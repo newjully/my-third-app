2e892447658c6c1fdcd1aef32cefeeab499b796e
Juliana da Cruz <juliana.da.cruz@avanade.com>
2023-02-18 18:11:53 -0300

    Ciclo de Vida

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
+import { motion } from 'framer-motion';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+      caracters: [],
+      loading: true
+    }
+  }
+
+
+  fetchCaracters = () =>{
+    console.log('Estou sendo Chamado')
+    fetch('https://rickandmortyapi.com/api/character')
+      .then(response => response.json()
+      .then(({ results}) => this.setState({caracters: results, loading: false})))
+  }
+
+  componentDidMount() {
+      console.log('componentDidMount')
+      setTimeout(() => {  
+        this.fetchCaracters() 
+      }, 1000)
+  }
+
+  render() {
+    console.log('render')
+    
+    const { caracters, loading} = this.state
+    
+    return (
+      <>
+      <div className='Container'>
+
+        <motion.div className='carousel' whileTop={{cursor:'grabbing'}}>.
+          <motion.div className='inner' drag='x' 
+          >
+            <h1>Personagens</h1>
+            { loading ? <h2>LOADING...</h2>   
+            : 
+            caracters.map(data => (
+          
+            <motion.div className='item' key={ data.name }>
+              <p>{ data.name }</p>
+              <img src={ data.image } alt={ data.name } />    
+            </motion.div>
+            ))
+            }
+          </motion.div>
+        </motion.div>
+
+      </div>
+      </>
+    );
+  }
 }
 
 export default App;
